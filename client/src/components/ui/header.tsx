bd68e651dd8ff06499625884ab5ded99d68b60e7
JerryShum <jerryshum2636@gmail.com>
2025-09-27 18:52:57 -0400

    feat: mapping routes instead of hardcoding

--- client/src/components/ui/header.tsx
+++ client/src/components/ui/header.tsx
@@ -4,22 +4,26 @@ import { Link } from '@tanstack/react-router';
 import github from '@/assets/github-mark.svg';
 
 export default function Header() {
+   const routes = [
+      { route: 'Home', path: '/' },
+      { route: 'Chat', path: '/chat' },
+      { route: 'AI Code Critic', path: '/ai' },
+      { route: 'Review', path: '/ai/review' },
+   ];
    return (
       <div className=" top-0 left-0 right-0 w-full p-4 px-10 flex justify-between items-center ">
          <div>
             <h1 className="text-2xl font-bold">ai_code_critic</h1>
          </div>
          <div className="flex gap-8">
-            <Button variant="link" className="text-black">
-               <Link to="/" className="text-[16px]">
-                  Home
-               </Link>
-            </Button>
-            <Button variant="link" className="text-black">
-               <Link to="/chat" className="text-[16px]">
-                  Chat
-               </Link>
-            </Button>
+            {routes.map((route) => (
+               <Button variant="link" key={route.path} className="text-black">
+                  <Link to={route.path} className="text-[16px]">
+                     {route.route}
+                  </Link>
+               </Button>
+            ))}
+
             <a
                href="https://github.com/JerryShum/ai-code-critic"
                target="_blank"
