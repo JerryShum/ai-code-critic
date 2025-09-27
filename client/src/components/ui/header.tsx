e6645ebf72fb191592d2c04cc3ec0c5fec48dee5
JerryShum <jerryshum2636@gmail.com>
2025-09-27 12:41:27 -0400

    feat: add logo and github link

--- client/src/components/ui/header.tsx
+++ client/src/components/ui/header.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Button } from './button';
 import { Link } from '@tanstack/react-router';
+import github from '@/assets/github-mark.svg';
 
 export default function Header() {
    return (
-      <div className="fixed top-0 left-0 right-0 w-full bg-black p-4 flex items-start">
-         <Button variant="link" className="text-white">
-            <Link to="/">Home</Link>
-         </Button>
-         <Button variant="link" className="text-white">
-            <Link to="/chat">Chat</Link>
-         </Button>
+      <div className="fixed top-0 left-0 right-0 w-full p-4 px-10 flex justify-between items-center ">
+         <div>
+            <h1 className="text-2xl font-bold">ai_code_critic</h1>
+         </div>
+         <div className="flex gap-8">
+            <Button variant="link" className="text-black">
+               <Link to="/" className="text-[16px]">
+                  Home
+               </Link>
+            </Button>
+            <Button variant="link" className="text-black">
+               <Link to="/chat" className="text-[16px]">
+                  Chat
+               </Link>
+            </Button>
+            <a
+               href="https://github.com/JerryShum/ai-code-critic"
+               target="_blank"
+               rel="noopener"
+            >
+               <img
+                  src={github}
+                  className="w-8  h-8 cursor-pointer"
+                  alt="github logo"
+               />
+            </a>
+         </div>
       </div>
    );
 }
